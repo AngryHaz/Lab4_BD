1159b674019849593e3e5ae2d9046443a834ad4a
AngryHaz <stardad88@gmail.com>
2020-11-30 21:18:20 +0500

    MACD && OBV

--- Lab4/Form1.cs
+++ Lab4/Form1.cs
@@ -21,9 +21,8 @@ namespace Lab4
         public static DataTable dataTable = new DataTable();
         public int global_iterator = 0;
         public int lenWindow = 10;  //Размер скользящего окна
-        
-        //SMA - Скользящее среднее. Формула расчета:
-        //https://allfi.biz/Forex/TechnicalAnalysis/Trend-Indicators/prostoe-skolzjashhee-srednee.php
+        public static int smoothing_MSA = 4; //Сглаживание скользящего среднего
+        public static int smoothing_MACD = 24;
 
         public Form1()
         {
@@ -66,29 +65,87 @@ namespace Lab4
             {
                 nrow[_dataT.Columns[cs].ColumnName.ToString()] = words[cs]; //По наименованию колонки устаналиваем значение в поле
             }
+            nrow["SMA"] = nrow["MACD"] = nrow["OBV"] = 0;
             _dataT.Rows.Add(nrow);
         }
-        static void set_valueSMA(int index_table)
-        {
-            int smoothing = 4; //Сглаживание
 
-            if (index_table < smoothing - 1)
+        static void set_value_Indicator(int index_table, string name_Indicator)
+        {
+            
+            double sum_value = 0;
+            double sum_value_MACD_bot = 0;
+            int bottom_line = 0;
+            if (index_table >= smoothing_MSA - 1 && name_Indicator == "SMA")
             {
-                dataTable.Rows[index_table].SetField("SMA", 0);
-                return;
+                //SMA - Скользящее среднее. Формула расчета:
+                //https://allfi.biz/Forex/TechnicalAnalysis/Trend-Indicators/prostoe-skolzjashhee-srednee.php
+                bottom_line = index_table - (smoothing_MSA - 1);
+                for (int i = bottom_line; i <= index_table; i++)
+                {
+                    sum_value += dataTable.Rows[i].Field<double>("CLOSE");
+                }
+                dataTable.Rows[index_table].SetField("SMA", sum_value / smoothing_MSA);
+            }else if(index_table >= smoothing_MACD - 1 && name_Indicator == "MACD")
+            {
+                //MACD 
+                //https://www.metatrader5.com/ru/terminal/help/indicators/oscillators/macd
+                bottom_line = index_table - (smoothing_MACD - 1);
+                for (int i = bottom_line; i <= index_table; i++)
+                {
+                    sum_value += dataTable.Rows[i].Field<double>("CLOSE");
+                    if (i == smoothing_MACD / 2 - 1)
+                    {
+                        sum_value_MACD_bot = sum_value;
+                    }
+                }
+                dataTable.Rows[index_table].SetField("MACD", (sum_value_MACD_bot / (smoothing_MACD / 2)) - (sum_value / smoothing_MACD));
+            }
+            else if(name_Indicator == "OBV" && index_table > 0)
+            {
+                
+                //OBV 
+                //https://www.metatrader5.com/ru/terminal/help/indicators/volume_indicators/obv
+                /*Если текущая цена закрытия выше предыдущей, то:
+
+                OBV(i) = OBV(i - 1) + VOLUME(i).
+
+                Если текущая цена закрытия ниже предыдущей, то:
+
+                OBV(i) = OBV(i - 1) - VOLUME(i)
+
+                Если текущая цена закрытия равна предыдущей, то:
+
+                OBV(i) = OBV(i - 1)
+
+                Где:
+
+                OBV(i) — значение индикатора On Balance Volume в текущем периоде;
+                OBV(i - 1) — значение индикатора On Balance Volume в предыдущем периоде;
+                VOLUME(i) — объем текущего бара. (Кол-во совершенных сделок за время «жизни» бара.)
+                */
+                double volume = 5; //Надо тянуть из файла
+                double previous_price = dataTable.Rows[index_table - 1].Field<double>("CLOSE");
+                double current_price = dataTable.Rows[index_table].Field<double>("CLOSE");
+                if (current_price > previous_price)
+                {
+                    dataTable.Rows[index_table].SetField("OBV", previous_price + volume);
+                }
+                else
+                {
+                    dataTable.Rows[index_table].SetField("OBV", previous_price - volume);
+                }
             }
             else
             {
-                double sum_value = 0;
-                //double SMA = 0;
-                int bottom_line = index_table - (smoothing - 1);
-                for (int i = bottom_line; i <= index_table; i++)
-                {
-                    sum_value += dataTable.Rows[i].Field<double>("OPEN");
-                }
-                dataTable.Rows[index_table].SetField("SMA", sum_value / smoothing);
+                return;
             }
         }
+
+        static void set_value_MACD(int index_table)
+        {
+
+        }
+
         static void InitializationDataTable()
         {
             /*  
@@ -144,18 +201,25 @@ namespace Lab4
                 series.Points.Clear();
             }
            
-            //OPEN
-            //ekran.Series[0].ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
-            
             //CLOSE
             ekran.Series[0].ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
-            ekran.Series[0].BorderWidth = 3;
+            ekran.Series[0].BorderWidth = 5;
             ekran.Series[0].Color = Color.Aqua;
 
             //SMA
             ekran.Series[1].ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
             ekran.Series[1].BorderWidth = 3;
-            ekran.Series[0].Color = Color.OrangeRed;
+            ekran.Series[1].Color = Color.OrangeRed;
+
+            //MACD
+            ekran.Series[2].ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            ekran.Series[2].BorderWidth = 3;
+            ekran.Series[2].Color = Color.Green;
+
+            //MACD
+            ekran.Series[3].ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            ekran.Series[3].BorderWidth = 3;
+            ekran.Series[3].Color = Color.Gold;
 
             const int N = 10;
             int j = 0;
@@ -163,11 +227,14 @@ namespace Lab4
             for (int i = 0; i < lenWindow; i++)
             {
                 //Задаем среднее скользящее
-                set_valueSMA(i + global_iterator);
+                set_value_Indicator(i + global_iterator, "SMA");
+                set_value_Indicator(i + global_iterator, "MACD");
+                set_value_Indicator(i + global_iterator, "OBV");
 
-                ekran.Series[0].Points.AddXY(j, dataTable.Rows[i + global_iterator].Field<double>("OPEN"));
+                ekran.Series[0].Points.AddXY(j, dataTable.Rows[i + global_iterator].Field<double>("CLOSE"));
                 ekran.Series[1].Points.AddXY(j, dataTable.Rows[i + global_iterator].Field<double>("SMA"));
-                //ekran.Series[1].Points.AddXY(j, dataTable.Rows[i + global_iterator].Field<double>("CLOSE"));
+                ekran.Series[2].Points.AddXY(j, dataTable.Rows[i + global_iterator].Field<double>("MACD"));
+                ekran.Series[3].Points.AddXY(j, dataTable.Rows[i + global_iterator].Field<double>("OBV"));
 
                 j += 1;
             }
